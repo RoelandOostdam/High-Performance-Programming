919a8d27c5f4fdb93e53078968ab793fd5c2a29e
Roeland <roeland.oostdam@outlook.com>
2020-02-05 10:38:11 +0100

    Reverse sorting & complexities

--- 1-1_sorting_complexity.py
+++ 1-1_sorting_complexity.py
@@ -9,7 +9,7 @@ ran2 = random.sample(range(0, 10000), 10000)
 
 def test(f):
     print("Testing", str(f))
-    results = {'s0':None, 's1':None, 's2':None}
+    results = {'s0':None, 's1':None, 's2':None, 's2r':None}
     time_start = datetime.datetime.now()
     f(ran0)
     results['s0'] = (datetime.datetime.now() - time_start).microseconds/1000
@@ -19,6 +19,10 @@ def test(f):
     time_start = datetime.datetime.now()
     f(ran2)
     results['s2'] = (datetime.datetime.now() - time_start).microseconds/1000
+    time_start = datetime.datetime.now()
+    ran2.reverse()
+    f(ran2)
+    results['s2r'] = (datetime.datetime.now() - time_start).microseconds / 1000
     return results
 
 timings = {
@@ -27,3 +31,8 @@ timings = {
     'sort_merge': test(sort_merge.merge_sort)
 }
 print(timings)
+
+# Complexity
+# Selection sort: Best: O(n^2), Average/Worst: O(n^2)
+# Insertion sort: Best: O(n) ,Average/Worst: O(n^2)
+# Merge sort: Best: O(n), Average/Worst: O(n log n)
