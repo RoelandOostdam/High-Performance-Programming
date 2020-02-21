761aa6c44717747e1887c6b6968e2508f32d86a9
Roeland Oostdam <Roeland.Oostdam@outlook.com>
2020-02-21 20:55:23 +0100

    Threaded merge sort

--- scripts/random_list.py
+++ scripts/random_list.py
@@ -1,5 +1,6 @@
 import random
 
-def gen_list(n):
-	print("Generating list of size",n)
+def gen_list(n, verbose=False):
+	if verbose:
+		print("Generating list of size",n)
 	return random.sample(range(0, n), n)
--- threaded_merge_sort.py
+++ threaded_merge_sort.py
@@ -1,4 +1,45 @@
-from scripts.sort_merge import recursive_merge_sort
+from scripts.sort_merge_custom import merge_sort, merge
+import scripts.random_list as random_list
+import numpy as np
+import concurrent.futures
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+import time
 
-arr = [5,4,2,7,1,4,7,3]
-print(recursive_merge_sort(arr))
+
+def combine_threads(tracker):
+	# TODO: This is not very efficient i know
+	while len(tracker) > 1:
+		tracker[0] = merge(tracker[0], tracker[1])
+		tracker.pop(1)
+	return tracker[0]
+
+
+def threaded_merge(arr, threads):
+	array_splits = np.array_split(arr, threads)
+	tracker = []
+	for t in range(0, threads):
+		tracker.append([])
+	with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+		for i, split in enumerate(array_splits):
+			future = executor.submit(merge_sort, list(split))
+			tracker[i] = future.result()
+	return combine_threads(tracker)
+
+
+arr = random_list.gen_list(10000)
+threads = [1,2,4,8]
+results = []
+for t in threads:
+	start_time = time.time()
+	threaded_merge(arr, t)
+	results.append(time.time() - start_time)
+
+plt.xticks(threads)
+plt.title("Time it takes to sort a list with multi-threading")
+plt.xlabel("Threads")
+plt.ylabel("Process duration (s)")
+plt.tight_layout()
+plt.grid()
+plt.plot(threads, results)
+plt.show()
